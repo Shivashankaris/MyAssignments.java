7c2ceb2b36d1d7288028896d8327a763c4b443e1
Shivashankaris <115482873+Shivashankaris@users.noreply.github.com>
2022-10-11 09:50:12 +0530

    Add files via upload

---
+++ JavaSelenium/src/JavaSelenium/src/main/java/TwoWheeler.java
@@ -0,0 +1,19 @@
+
+
+public class TwoWheeler{
+	public static void main(String[] args) {
+		int noofWheels=2;
+		System.out.println(noofWheels);
+		short noofMirrors=2;
+		System.out.println(noofMirrors);
+		long chassisNumber=22657843238234L;
+		System.out.println(chassisNumber);
+		boolean ispunctured=false;
+		System.out.println(ispunctured);
+		String bikeName="R15";
+		System.out.println(bikeName);
+		double runningKM=43.876;
+		System.out.println(runningKM);
+	}
+
+}
---
+++ JavaSelenium/src/JavaSelenium/src/main/java/mobile.java
@@ -0,0 +1,25 @@
+
+public class mobile {
+	
+	String mobileBrandName = "Realme";
+	char mobileLogo = 'R';
+	short noofMobilePieces = 50;
+	int modelNumber = 1;
+	long mobileImeiNumber =  6543476896542L;
+	float mobilePrice =  27879.99F;
+	boolean mobileIsDamaged = false;
+	
+	public static void main(string[] args) {
+		Mobile mobile = new mobile();
+System.out.println("Brand Name="+mobile. mobileBrandName);
+System.out.println("Mobile Logo="+mobile. mobileLogo);
+System.out.println("noofmobiles="+mobile.noofmobilePieces);
+System.out.println("Modelno="+mobile.modelNumber);
+System.out.println("IMEI no ="+mobile.mobileImmeiNumber);
+System.out.println("Mobile Price="+mobile.mobilePrice);
+System.out.println(Is damaged="+mobile.mobileIsDamaged");
+
+	}
+	
+
+}
