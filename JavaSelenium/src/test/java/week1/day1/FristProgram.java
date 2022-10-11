f3b2377c795eac6fdb9c4bd18d8912dfbc27acad
Shivashankaris <115482873+Shivashankaris@users.noreply.github.com>
2022-10-11 09:40:19 +0530

    Add files via upload

---
+++ JavaSelenium/src/test/java/week1/day1/FristProgram.java
@@ -0,0 +1,8 @@
+package week1.day1;
+
+public class FristProgram {
+public static void main(String[] args) {
+	
+	
+}
+}
---
+++ JavaSelenium/src/test/java/week1/day1/bycle.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class bycle {
+	public static void main(String[] args) {
+		String cycleName="Hercules";
+		System.out.println(cycleName);
+		short noofPedals=2;
+		System.out.println( noofPedals);
+		int noofwheels=2;
+		System.out.println(noofwheels);
+		char firstLetter='H';
+				System.out.println(firstLetter);
+		float cyclefrequency=213.4f;
+		System.out.println(cyclefrequency);
+		double cyclePrices=245.98983;
+		System.out.println(cyclePrices);
+		boolean ispuncture=false;
+		System.out.println(ispuncture);
+	}
+		
+	
+	
+		
+	
+		
+
+
+			
+	
+	
+}
