7dee659d771bcbae0d5956f7cac87e87391a6554
Shivashankaris <115482873+Shivashankaris@users.noreply.github.com>
2022-10-13 08:29:16 +0530

    Add files via upload

---
+++ day3/Prime.java
@@ -0,0 +1,27 @@
+package week.day3;
+
+public class Prime {
+	public static void main(String[] args) {
+		 int n = 7;
+		    int count = 0;
+		    if (n <= 1) {
+		      System.out.println("The number is not prime");
+		      return;
+		    }
+		    for (int i = 1; i <= n / 2; i++) {
+		      if (n % i == 0) {
+		        count++;
+		      }
+		    }
+
+		    if (count > 1) {
+		        System.out.println("The number is not prime");
+		    }
+		    else {
+		      System.out.println("The number is prime");
+		    }
+		  }
+
+		
+	}
+
